19c5fa6d4d14b51100f703b5b8d862b999800180
bazzan <bazzanapalowo@gmail.com>
2022-01-19 13:56:29 +0100

    fr: inventory edit and delete

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
-from .views import ProductList, ProductCreate, ProductDetail
+from . import views
 
 app_name = "products"
 urlpatterns = [
-    path('', ProductList.as_view(), name="home"),
-    path('new/', ProductCreate.as_view(), name="create"),
-    path('<str:pk>/', ProductDetail.as_view(), name="detail")
+    path('', views.ProductList.as_view(), name="home"),
+    path('new/', views.ProductCreate.as_view(), name="create"),
+    path('<str:pk>/', views.ProductDetail.as_view(), name="detail"),
+    path('<str:pk>/edit/', views.ProductEdit.as_view(), name="edit"),
+    path('<str:pk>/delete/', views.ProductDelete.as_view(), name="delete"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Product
 from django.views import generic
 from django.urls import reverse_lazy
@@ -18,7 +18,36 @@ class ProductCreate(generic.CreateView):
     success_url = reverse_lazy('products:home')
     fields = ['name']
 
+    def get_context_data(self, **kwargs):
+        kwargs["type"] = "Create"
+        return super().get_context_data(**kwargs)
+
 
 class ProductDetail(generic.DetailView):
     template_name = "products/detail.html"
     queryset = Product.objects.all()
+
+
+class ProductEdit(generic.UpdateView):
+    queryset = Product.objects.all()
+    template_name = "products/new.html"
+    fields = ['name']
+
+    def get_success_url(self):
+        return reverse_lazy('products:detail', args=(self.kwargs["pk"]))
+
+    def get_context_data(self, **kwargs):
+        kwargs["type"] = "Edit"
+        return super().get_context_data(**kwargs)
+
+
+class ProductDelete(generic.DeleteView):
+    success_url = reverse_lazy("products:home")
+    template_name = None
+
+    def get_object(self, queryset=None):
+        return Product.objects.filter(id=self.kwargs["pk"])
+
+    def render_to_response(self, context, **response_kwargs):
+        self.get_object().delete()
+        return HttpResponseRedirect(self.success_url)
